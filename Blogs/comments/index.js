e79e8a30ffef63945f26a0971d4b80863daaf1b9
Awal hossain <waqin21@gmail.com>
2023-02-05 11:07:39 +0600

    comments id & content api 🔥🔥

---
+++ Blogs/comments/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser  = require('body-parser');
+const {randomBytes}  = require('crypto');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res)=>{
+    
+    return res.send(commentsByPostId);
+})
+
+app.post("/posts/:id/comments", (req, res)=>{
+    const commentId  = randomBytes(4).toString('hex');
+    const {content}  = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+
+   
+    comments.push({id: commentId, content});
+    commentsByPostId[req.params.id] = comments;
+
+    return res.status(201).send(posts[id])
+})
+
+
+app.listen(4000, ()=>{
+    console.log(`This app is listenning on 4000`);
+})
