a0ff5478ddecc3f7d1cf15b7538771c0c765a98f
Awal hossain <waqin21@gmail.com>
2023-02-06 15:33:24 +0600

    event-bus introduce 🆕🆕

---
+++ Blogs/event-bus/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser  = require('body-parser');
+const {randomBytes}  = require('crypto');
+const { default: axios } = require("axios");
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const events = [];
+
+app.post("/event",async(req, res)=>{
+    const event = req.body;
+
+    events.push(event);
+   
+ await axios.post('http://localhost:4000/events', event);
+ await axios.post('http://localhost:4001/events', event);
+ await axios.post('http://localhost:4002/events', event);
+ await axios.post('http://localhost:4003/events', event);
+
+ return res.send({status: "OK"});
+})
+
+
+app.get("/events", (req, res)=>{
+    res.send(events);
+})
+
+
+app.listen(4005, ()=>{
+    console.log(`This app is running on 4005`);
+})
