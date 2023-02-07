c76b22aed28ca2849878f1fa3af71e2365648358
Awal hossain <waqin21@gmail.com>
2023-02-07 21:38:07 +0600

    query server introduced 🔥🔥

--- Blogs/query/index.js
+++ Blogs/query/index.js
@@ -9,6 +9,36 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
+const posts = {};
+
+app.get("/posts",()=>{
+    res.send(posts)
+})
+
+
+app.post("/events", (req,res)=>{
+    const {type, data} = req.body;
+
+    if(type == "PostCreated"){
+        const {id, title} = data;
+        posts[id] = {
+            id,
+            title,
+            comments: []
+        }
+    }
+
+    if(type === "CommentCreated"){
+        const {id, content, postID}  = data;
+        const post = posts[postID];
+
+        post.comments.push({id, content});
+    }
+
+    console.log(posts)
+    res.send({status:"ok"})
+
+})
 
 
 
